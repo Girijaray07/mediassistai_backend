1f12981af301a751f4f661660f9d3765cebdce1d
Girija Ray <91793906+Girijaray07@users.noreply.github.com>
2026-05-11 12:14:20 +0530

    Cleaned Backend

--- backend/db/__init__.py
+++
@@ -1,6 +0,0 @@
-"""=========================================
-   DB Modules contains the connection to Database and Session
-   return connection or raise Error
-   Ensure DATABASE_URL is initialized in .env
-   =========================================
-"""
